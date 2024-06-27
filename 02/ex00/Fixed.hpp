9a33a3315798f0f9acdaa8763e83e93d4cde14c0
7h3Y055 <youssefbouchmama18@outlook.com>
2024-06-27 15:21:48 +0100

    tmp

--- 02/ex00/Fixed.hpp
+++ 02/ex00/Fixed.hpp
@@ -1,15 +1,20 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
 #include <iostream>
 
 class Fixed
 {
-private:
-    int fixed_point;
-    static const int  fractional_bits = 8;
-public:
-    Fixed();
-    Fixed(const Fixed &obj);
-    Fixed& operator=( const Fixed &rhs );
-    ~Fixed();
-    int getRawBits( void ) const;
-    void setRawBits( int const raw );
+    private:
+        int fixed_point;
+        static const int  fractional_bits = 8;
+    public:
+        Fixed();
+        Fixed(const Fixed &obj);
+        Fixed& operator=( const Fixed &rhs );
+        ~Fixed();
+        int getRawBits( void ) const;
+        void setRawBits( int const raw );
 };
+
+#endif
